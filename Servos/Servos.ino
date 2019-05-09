f24287d2ebb6623bf3e60f8e02da0e252fe0e642
Buddhika P. De Silva <bevylabs@gmail.com>
2019-05-09 09:38:32 +0530

    modify servor motor code and bluetooth

--- Servos/Servos.ino
+++ Servos/Servos.ino
@@ -1,13 +1,12 @@
-int motorPin1 = 8;    // Blue   - 28BYJ48 pin 1
-int motorPin2 = 9;    // Pink   - 28BYJ48 pin 2
-int motorPin3 = 10;    // Yellow - 28BYJ48 pin 3
-int motorPin4 = 11;    // Orange - 28BYJ48 pin 4
-// Red    - 28BYJ48 pin 5 (VCC)
-int motor2Pin1 = 4;    // Blue   - 28BYJ48 pin 1
-int motor2Pin2 = 5;    // Pink   - 28BYJ48 pin 2
-int motor2Pin3 = 6;    // Yellow - 28BYJ48 pin 3
-int motor2Pin4 = 7;    // Orange - 28BYJ48 pin 4
-// Red    - 28BYJ48 pin 5 (VCC)
+#include <Servo.h>
+
+Servo servo;
+
+Servo servo2;
+
+int x = 30;
+boolean stat = false;
+boolean ON = false;
 //M1
 int enA = A0;
 int in1 = 2;
@@ -27,16 +26,8 @@ int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001}
 //////////////////////////////////////////////////////////////////////////////
 void setup() {
   //declare the motor pins as outputs
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  pinMode(motorPin3, OUTPUT);
-  pinMode(motorPin4, OUTPUT);
-
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
-  pinMode(motor2Pin3, OUTPUT);
-  pinMode(motor2Pin4, OUTPUT);
-
+servo.attach(9);
+servo2.attach(10);
 
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
@@ -159,55 +150,26 @@ void loop() {
 //delay "motorSpeed" between each pin setting (to determine speed)
 void anticlockwise()
 {
-  for (int i = 0; i < 8; i++)
+  for (int i = 0; i < 150; i++)
   {
-    setOutput(i);
-    delayMicroseconds(motorSpeed);
+   
+    servo.write(i);
+
+    delay(50);
+    // delay(2000);
+
+
   }
 }
 
 void clockwise()
 {
-  for (int i = 7; i >= 0; i--)
+  for (int i = 150; i >= 0; i--)
   {
-    setOutput(i);
-    delayMicroseconds(motorSpeed);
+    servo.write(i);
+
+    delay(50);
+    // delay(2000);
+
   }
-}
-
-void anticlockwise2()
-{
-  for (int i = 0; i < 8; i++)
-  {
-    setOutput2(i);
-    delayMicroseconds(motorSpeed);
-  }
-}
-
-void clockwise2()
-{
-  for (int i = 7; i >= 0; i--)
-  {
-    setOutput2(i);
-    delayMicroseconds(motorSpeed);
-  }
-}
-
-
-
-
-void setOutput(int out)
-{
-  digitalWrite(motorPin1, bitRead(lookup[out], 0));
-  digitalWrite(motorPin2, bitRead(lookup[out], 1));
-  digitalWrite(motorPin3, bitRead(lookup[out], 2));
-  digitalWrite(motorPin4, bitRead(lookup[out], 3));
-}
-
-void setOutput2(int out)
-{
-  digitalWrite(motor2Pin1, bitRead(lookup[out], 0));
-  digitalWrite(motor2Pin2, bitRead(lookup[out], 1));
-  digitalWrite(motor2Pin3, bitRead(lookup[out], 2));
-  digitalWrite(motor2Pin4, bitRead(lookup[out], 3));
 }
